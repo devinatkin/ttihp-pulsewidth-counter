2f6608f927feee27553efca4fe588d84a4a25701
Devin Atkin <devinatkin@gmail.com>
2024-11-04 02:48:15 +0000

    Remove Duplicate Driver

--- src/tt_um_devinatkin_pulse_width_counter.v
+++ src/tt_um_devinatkin_pulse_width_counter.v
@@ -28,7 +28,6 @@ module tt_um_devinatkin_pulse_width_counter (
   parameter sel_width = $clog2(4);
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
